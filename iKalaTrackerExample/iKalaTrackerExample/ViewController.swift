e9628c5d376f1bf8aae4ae510feec66b7e60ca9e
allenlinli <allenlinli@gmail.com>
2020-03-26 15:54:58 +0800

    chore: add an iOS project as the example project

---
+++ iKalaTrackerExample/iKalaTrackerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iKalaTrackerExample
+//
+//  Created by Allen and Kim on 2020/3/26.
+//  Copyright © 2020 ikala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
